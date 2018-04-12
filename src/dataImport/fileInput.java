23d03672cbc68e5bcc9cf1ada662d852f8605c1e
arlefas <alefas@auth.gr>
2018-04-13 02:38:25 +0300

    dataInput works

--- src/dataImport/fileInput.java
+++ src/dataImport/fileInput.java
@@ -1,8 +1,55 @@
 package dataImport;
 
-public class fileInput {
-String fileName;
-String fileCode;
+import java.io.File;
+import org.apache.commons.io.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 
+
+public class fileInput {
+	public String fileName;
+	public String fileCode;
+
+	public static fileInput[] createFileInput(File rootDir) {
+			String extensions[] = {"java" , "py"};
+			Collection<File> files= FileUtils.listFiles(rootDir, extensions , true);
+			File[] filesArray = files.toArray(new File[files.size()]);
+
+			int size = filesArray.length;
+
+			fileInput[] inputArray = new fileInput[size];
+
+			for (int i=0; i<size; i++) {	
+				inputArray[i] = new fileInput();
+				inputArray[i].fileName = filesArray[i].getName();
+				inputArray[i].fileCode = readFile(filesArray[i]);
+
+			}
+			return inputArray;
+	}
+
+	private static String readFile(File file) {
+		try {
+			return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public fileInput() {
+		fileName="";
+		fileCode="";
+	}
 
 }
+
+
+
+
+
+
+
+
+
---
+++ src/sourceCodeSemanticClustering/SourceCodeSemanticClustering.java
@@ -0,0 +1,25 @@
+package sourceCodeSemanticClustering;
+
+import java.io.File;
+
+import dataImport.fileInput;
+
+public class SourceCodeSemanticClustering {
+
+	public static void main(String[] args) {
+			File projectDir=new File(("c:\\project"));
+			fileInput[] fileIn = fileInput.createFileInput(projectDir);
+			int size = fileIn.length;
+			for (int i=0; i<size; i++) {	
+				System.out.println(fileIn[i].fileName);
+				System.out.println(fileIn[i].fileCode);
+
+			}
+		}
+
+
+	
+
+	
+
+}
