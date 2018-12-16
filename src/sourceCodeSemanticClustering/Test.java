2d55986ea90a692962fde1b601cf5261bdfd7a61
mkrasanakis <maniospas@hotmail.com>
2018-12-16 21:32:34 +0200

    testing changes

--- src/sourceCodeSemanticClustering/Test.java
+++ src/sourceCodeSemanticClustering/Test.java
@@ -10,6 +10,7 @@ import dataImport.FileInput;
 import dataImport.ProjectInput;
 import featureExtraction.WordModelFeatureExtraction;
 import featureExtraction.WordModelFeatureExtractionAddedWeight;
+import weka.core.pmml.jaxbbindings.Cluster;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
@@ -20,7 +21,7 @@ public class Test {
 		WordModelFeatureExtraction features = new WordModelFeatureExtractionAddedWeight(project.getInput(), new featureExtraction.featureWeight.TermFrequencyInverseDocumentFrequencyWeight(), wordModel, 1, 1);
 		
 		//Clustering algorithm = new clustering.algorithms.KmeansDynamic(features.getOccurenceTable(), new clustering.evaluation.IntraSimilarity(new clustering.distance.CosineDistance()), new clustering.distance.CosineDistance());
-		Clustering algorithm = new clustering.algorithms.Kmeans(features.getOccurenceTable(), 20, new clustering.distance.CosineDistance());
+		Clustering algorithm = new clustering.algorithms.TopicsKmeans(features.getOccurenceTable(), 10, 20, new clustering.distance.CosineDistance());
 		Evaluation[] metrics = {new clustering.evaluation.Precision(evaluationClusters), new clustering.evaluation.Recall(evaluationClusters)};
 		
 		int[] clusters = algorithm.returnClusters();
