3d2c09353e80baf5d9ac6b9af8dc66a10303794b
mkrasanakis <maniospas@hotmail.com>
2018-12-18 17:57:08 +0200

    tests

--- src/sourceCodeSemanticClustering/Test.java
+++ src/sourceCodeSemanticClustering/Test.java
@@ -15,14 +15,14 @@ import weka.core.pmml.jaxbbindings.Cluster;
 public class Test {
 	public static void main(String[] args) throws Exception {
 		WordModel wordModel = new WordModel.BagOfWords(new auth.eng.textManager.stemmers.InvertibleStemmer(new auth.eng.textManager.stemmers.PorterStemmer()));
-		ProjectInput project = new ProjectInput(FileInput.createFileInput(new File("../_repos/smile")), "THIS");
-		int[] evaluationClusters = (new PackagesToClusters()).Clusters(new File("../_repos/smile"));
+		ProjectInput project = new ProjectInput(FileInput.createFileInput(new File("./")), "THIS");
+		int[] evaluationClusters = (new PackagesToClusters()).Clusters(new File("./"));
 		
 		WordModelFeatureExtraction features = new WordModelFeatureExtractionAddedWeight(project.getInput(), new featureExtraction.featureWeight.TermFrequencyInverseDocumentFrequencyWeight(), wordModel, 1, 1);
 		
 		//Clustering algorithm = new clustering.algorithms.KmeansDynamic(features.getOccurenceTable(), new clustering.evaluation.IntraSimilarity(new clustering.distance.CosineDistance()), new clustering.distance.CosineDistance());
-		Clustering algorithm = new clustering.algorithms.TopicsKmeans(features.getOccurenceTable(), 10, 20, new clustering.distance.CosineDistance());
-		Evaluation[] metrics = {new clustering.evaluation.Precision(evaluationClusters), new clustering.evaluation.Recall(evaluationClusters)};
+		Clustering algorithm = new clustering.algorithms.TopicsKmeans(features.getOccurenceTable(), evaluationClusters.length, 20, new clustering.distance.CosineDistance());
+		Evaluation[] metrics = {new clustering.evaluation.Precision(evaluationClusters), new clustering.evaluation.Recall(evaluationClusters), new clustering.evaluation.MojoFM(evaluationClusters)};
 		
 		int[] clusters = algorithm.returnClusters();
 		for(Evaluation metric : metrics) 
